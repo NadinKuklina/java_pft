443d05b041fb83cfd0809c8e547aaf8eb890641a
NKU <nkuqa@mail.ru>
2020-05-07 12:17:28 +0300

    Созданы классы и преобразованы методы.

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+/* Created by NKU on 07.05.2020 */
+
+public class Rectangle {
+  /*атрибуты класса*/
+  public double a;
+  public double b;
+
+  /*конструктор класса*/
+  public Rectangle(double a, double b){
+    this.a = a;
+    this.b = b;
+  }
+}
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sandbox;
+/* Created by NKU on 07.05.2020 */
+
+public class Square {
+
+  public double l;
+
+  /*конструктор класса*/
+  public Square(double l){
+    this.l = l;
+  }
+}
